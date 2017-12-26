9298bef022139880a1251a36d07a4eab1a0aefff
liyil <1041965191@qq.com>
2017-12-26 22:24:58 +0800

    test

---
+++ spring-cloud-config/src/main/java/com/liyl/Application.java
@@ -0,0 +1,17 @@
+package com.liyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 李毅霖.
+ * @Description:
+ * @Date: 2017/12/24
+ * @Modified by .
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
