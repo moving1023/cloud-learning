c9421336157503ab7fb558366e5a4c64cca16082
liyil <1041965191@qq.com>
2017-12-26 22:28:46 +0800

    test

--- spring-cloud-eureka/src/main/java/com/liyl/Application.java
+++ spring-cloud-eureka/src/main/java/com/liyl/Application.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
